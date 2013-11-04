a260eb0833426885c20eefe3e0bdb98968231dcd
Erick Mendonca <erickmendonca@yahoo.com.br>
2013-11-04 14:27:00 -0300

    Adiciona projeto inicial

---
+++ Hello World, Kinect!/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Hello_World__Kinect_
+{
+    /// <summary>
+    /// Interação lógica para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
